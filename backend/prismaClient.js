1f7cb5262c2a2d28e28c76ae06dcfebc64f5837d
Aditya Sonkar <adityasonkar1204@gmail.com>
2026-07-02 05:15:01 +0530

    initial commit

--- backend/prismaClient.js
+++
@@ -1,2 +0,0 @@
-// Deprecated: Prisma client generation is now handled in src/prismaClient.ts
-export {};
--- backend/server.js
+++
@@ -1,3 +0,0 @@
-// This monolithic server file is fully deprecated and its logic has been 
-// cleanly separated into the TypeScript files in the src/ directory.
-// You can safely ignore or delete this file.
